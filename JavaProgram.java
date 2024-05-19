c1ba9974bf959defb3d485bd494e5301a2178a9c
Tania1511 <taniarajput.scorpio@gmail.com>
2024-05-19 13:59:25 +0530

    added one more line

--- JavaProgram.java
+++ JavaProgram.java
@@ -3,5 +3,6 @@ public class JavaProgram{
     public static void main(String[] args){
         System.out.println("Hello jenkins");
         System.out.println(new Date());
+        System.out.println("Learn to grow");
     }
 }
