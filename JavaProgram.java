e2092bed3ac288b26c145f6149b93a20dd6dcc96
Tania1511 <taniarajput.scorpio@gmail.com>
2024-05-19 13:38:47 +0530

    Initial commit

---
+++ JavaProgram.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class JavaProgram{
+    public static void main(String[] args){
+        System.out.println("Hello jenkins");
+        System.out.println(new Date());
+        System.out.println("new learning");
+    }
+}
